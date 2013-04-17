7540ed27be943ec7ee1cb776fe006a1dd4c5ab6c
Manuel Zambrano <mmzmm3z@gmail.com>
2013-04-17 00:10:08 -0430

    archivo que se encarga de almacenar todas las configuaraciones

---
+++ src/web/core/config/main.php
@@ -0,0 +1,17 @@
+<?php
+/*
+* @file main.php
+* Archivo encargado de guardar las configuraciones
+*/
+
+return array(
+	'db'=>array(
+		//Agregar direccion IP o DNS del servidor de base de datos
+		'server'=>'localhost',
+		//nombre del usuario a utilizar para la conexion
+		'user'=>'root',
+		//palabra clave del usuario del servidor de base de datos
+		'password'=>'1234',
+		),
+	);
+?>
